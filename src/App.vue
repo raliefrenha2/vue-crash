0fc7330b51656ca36521166709857c4402b1d880
Romi Alief Rahman <romi.alief@infokes.co.id>
2025-01-25 10:50:13 +0700

    Add HomeCards Component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup>
 import Hero from "./components/Hero.vue";
+import HomeCards from "./components/HomeCards.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 <template>
   <Navbar />
   <Hero title="Test Tittle" subtitle="Subtitle Test" />
+  <HomeCards />
 </template>
