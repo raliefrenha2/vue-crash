8974401ac1dae1af00edd7f7d6402d89d7b8fed9
Romi Alief Rahman <romi.alief@infokes.co.id>
2025-01-25 10:25:43 +0700

    add Hero Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup>
+import Hero from "./components/Hero.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 <template>
   <Navbar />
+  <Hero title="Test Tittle" subtitle="Subtitle Test" />
 </template>
