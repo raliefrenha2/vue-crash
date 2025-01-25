c3dab0636c5a73d44bfa6f4fa2a3a0763333f9b7
Romi Alief Rahman <romi.alief@infokes.co.id>
2025-01-25 13:38:18 +0700

    add JobListings Component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import Hero from "./components/Hero.vue";
 import HomeCards from "./components/HomeCards.vue";
+import JobListings from "./components/JobListings.vue";
 import Navbar from "./components/Navbar.vue";
 </script>
 <template>
   <Navbar />
   <Hero title="Test Tittle" subtitle="Subtitle Test" />
   <HomeCards />
+  <JobListings />
 </template>
