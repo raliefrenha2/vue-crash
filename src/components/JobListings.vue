dd8cecbcd7aa051af9150198be79c3536683c351
Romi Alief Rahman <romi.alief@infokes.co.id>
2025-01-25 17:47:44 +0700

    add JobsView

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -11,8 +11,6 @@ defineProps({
   },
 });
 const jobs = ref(jobData);
-
-console.log(jobs.value);
 </script>
 <template>
   <section class="bg-blue-100 px-4 py-10">
@@ -22,7 +20,7 @@ console.log(jobs.value);
       </h2>
       <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
-          v-for="job in jobs.slice(limit || jobs.length)"
+          v-for="job in jobs.slice(0, limit || jobs.length)"
           :key="job.id"
           :job="job"
         />
